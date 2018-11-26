23a86c9e98f8b6c1dbfd5564cbab321f3e2386cb
Simone Ray <44311795+sim1ray@users.noreply.github.com>
2018-11-26 13:00:50 -0800

    Update Load_tables.sql

--- Load_tables.sql
+++ Load_tables.sql
@@ -9,40 +9,47 @@ LOAD DATA LOCAL INFILE "/Users/simone/Downloads/Housing-Database/Housing_Databas
 INTO TABLE ADMINISTRATOR
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
 ;
 
 LOAD DATA LOCAL INFILE "/Users/simone/Downloads/Housing-Database/Housing_Database - Housing_unit.csv" 
 INTO TABLE HOUSING_UNIT
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
 ;
 
 LOAD DATA LOCAL INFILE "/Users/simone/Downloads/Housing-Database/Housing_Database - Resident.csv" 
 INTO TABLE RESIDENT
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
 ;
 
 LOAD DATA LOCAL INFILE "//Users/simone/Downloads/Housing-Database/Housing_Database - Applicant.csv" 
 INTO TABLE APPLICANT
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
 ;
 
 LOAD DATA LOCAL INFILE "/Users/simone/Downloads/Housing-Database/Housing_Database - Application.csv" 
 INTO TABLE APPLICATION
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
 ;
 
 LOAD DATA LOCAL INFILE "/Users/simone/Downloads/Housing-Database/Housing_Database - Housing_preference.csv" 
 INTO TABLE HOUSING_PREFERENCE
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
 ;
 
 LOAD DATA LOCAL INFILE "/Users/simone/Downloads/Housing-Database/Housing_Database - Maintenance_request.csv" 
 INTO TABLE MAINTENANCE_REQUEST
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
 ;
