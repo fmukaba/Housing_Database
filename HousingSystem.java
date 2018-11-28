b106d01fe8ff0ec5d783867d70664153f35b6169
Simone Ray <simray84@gmail.com>
2018-11-27 16:23:30 -0800

    Final!

--- HousingSystem.java
+++ HousingSystem.java
@@ -18,26 +18,32 @@ public class HousingSystem {
 
     // Runs the queries based on the input
     public ArrayList<HousingUnit> checkAvailability() throws SQLException {
-        ArrayList<HousingUnit> units = new ArrayList<>();
-
+        ArrayList<HousingUnit> units = new ArrayList<HousingUnit>();
         String query = "SELECT DISTINCT Building_number, Bedroom_number, Housing_type, Married_couples_allowed," +
                 "Price_quarter, count(*) from HOUSING_UNIT where Occupation_status = 0 group by Building_number, " +
                 "Bedroom_number, Housing_type, Married_couples_allowed, Price_quarter";
         PreparedStatement p = conn.prepareStatement(query);
+
         ResultSet r = p.executeQuery();
+
         while (r.next()) {
             int building = r.getInt(1);
+
             int bedrooms = r.getInt(2);
+
             String type = r.getString(3);
+
             boolean married = r.getBoolean(4);
+
             int price = r.getInt(5);
             HousingUnit hu = new HousingUnit(building, bedrooms, type, married, price);
             units.add(hu);
         }
+
         return units;
     }
 
-    public void createApplicant(String ID, String username, String password, String name, String gender,
+    public void createUser(String ID, String username, String password, String name, String gender,
                                 boolean studentStatus, boolean maritalStatus, String address, String phoneNumber,
                                 String college, String department, String major, String familyHeadID)
             throws SQLException {
@@ -72,7 +78,7 @@ public class HousingSystem {
         PreparedStatement p = conn.prepareStatement(query);
         p.setString(1, roommate);
         ResultSet r = p.executeQuery();
-        if (r.next()) {
+        if (r.next()) {                             // roommate was found in the system
             int occupied = r.getInt(1);
             if (occupied == 0) {
                 aptNo = r.getInt(2);
@@ -99,7 +105,10 @@ public class HousingSystem {
                 }
             }
         }
-        return false; //Booking did not go through
+
+        //Booking did not go through
+        createApplicant(SID, preferences, roommate);
+        return false;
     }
 
     // If Booking goes through, add Resident
@@ -117,20 +126,22 @@ public class HousingSystem {
         p2.setString(2, adminStaffID);
         p2.setInt(3, bNo);
         p2.setInt(4, aptNo);
+        p2.execute();
 
         // Change occupation status if unit gets filled
-        String query3 = "SELECT Count(*) from RESIDENT where Apt_number = ?";
+        String query3 = "SELECT Count(*) from RESIDENT where Apt_number = ?"; // how many current residents in unit
         PreparedStatement p3 = conn.prepareStatement(query3);
         p2.setInt(1, aptNo);
         ResultSet r3 = p3.executeQuery();
         int current = r3.getInt(1);
 
-        String query4 = "SELECT Max_people from HOUSING_UNIT where Apt_number = ?";
+        String query4 = "SELECT Max_people from HOUSING_UNIT where Apt_number = ?"; // max number of residents in unit
         PreparedStatement p4 = conn.prepareStatement(query4);
         p4.setInt(1, aptNo);
         ResultSet r4 = p4.executeQuery();
         int max = r4.getInt(1);
 
+        // check if unit is filled to capacity
         if (current == max) {
             String query5 = "UPDATE HOUSING_UNIT SET Occupation_status = 1 where Apt_number = ?";
             PreparedStatement p5 = conn.prepareStatement(query5);
@@ -140,13 +151,37 @@ public class HousingSystem {
 
     }
 
+    // Add applicant who did not get into the system (wait list)
+    public static void createApplicant(String ID, ArrayList<HousingUnit> preferences, String roommate) throws SQLException {
+
+        String query = "INSERT INTO APPLICANT(ID_number, Acceptance_status) VALUES (?, 0)";
+        PreparedStatement p = conn.prepareStatement(query);
+        p.setString(1, ID);
+        p.execute();
+
+        int index = 1;
+        for (HousingUnit h : preferences) {
+            String query2 = "INSERT INTO HOUSING_PREFERENCES(ID_number, Building_preference, Housing_type_preference, " +
+                    "Bedroom_preference, Order_of_preference, Roommate_preference) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement p2 = conn.prepareStatement(query2);
+            p2.setString(1, ID);
+            p2.setInt(2, h.getBuilding());
+            p2.setString(3, h.getType());
+            p2.setInt(4, h.getBedrooms());
+            p2.setInt(5, index);
+            p2.setString(6, roommate);
+            p2.execute();
+
+            index++;
+        }
+    }
+
     public static ArrayList<MaintenanceRequestDue> runReports() {
         ArrayList<MaintenanceRequestDue> list = new ArrayList<MaintenanceRequestDue>();
         String CurrentDate = "";
         System.out.println("Active maintenance requests: ");
 
         try {
-
             String query = "SELECT USER.Name, Building_number, Apt_number, Submission_date, Date_Completed, Comments\r\n"
                     + "FROM RESIDENT, MAINTENANCE_REQUEST, USER\r\n"
                     + "WHERE RESIDENT.ID_NUMBER = USER.ID_NUMBER and Submission_date <= ? "; // currentDate
