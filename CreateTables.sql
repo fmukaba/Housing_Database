fe414b0a048611019383811c014f4e2871b07b2d
Francois (Franck) Mukaba <fxkikomina@gmail.com>
2018-11-28 11:32:30 -0800

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -16,7 +16,7 @@ College             VARCHAR(50),
 Department          VARCHAR(50),
 Major               VARCHAR(50),
 Family_head_ID      CHAR(9),
-                                                                      
+
 PRIMARY KEY (ID_number)
 );
 
@@ -24,30 +24,30 @@ ALTER TABLE USER ADD FOREIGN KEY(Family_head_ID) references USER(ID_number) on d
 
 CREATE TABLE ADMINISTRATOR (
 
-Staff_ID              CHAR(9)            NOT NULL,
-SSN                   CHAR(9),
-Admin_Name            VARCHAR(50),
-Gender                CHAR(1)            CHECK (Gender IN('F', 'M')),
-Phone_number          VARCHAR(50),
-Job_title             VARCHAR(50),
-ResidentAdmin_flag    BOOLEAN,
-MaintenanceAdmin_flag BOOLEAN,
-AdmissionsAdmin_flag  BOOLEAN,
+Staff_ID               CHAR(9)            NOT NULL,
+SSN                    CHAR(9),
+Admin_Name             VARCHAR(50),
+Gender                 CHAR(1)            CHECK (Gender IN('F', 'M')),
+Phone_number           VARCHAR(50),
+Job_title              VARCHAR(50),
+ResidentAdmin_flag     BOOLEAN,
+MaintenanceAdmin_flag  BOOLEAN,
+AdmissionsAdmin_flag   BOOLEAN,
 
 PRIMARY KEY (Staff_ID)
 );
 
 CREATE TABLE HOUSING_UNIT (
 
-Building_number         INT     NOT NULL,
-Apt_number              INT     NOT NULL,
-Bedroom_number          INT,
-Max_people              INT,
-Married_couples_allowed BOOLEAN,
-Bed_number              INT,
-Housing_type            VARCHAR(50),
-Occupation_status       BOOLEAN,
-Price_quarter		    INT,
+Building_number             INT     NOT NULL,
+Apt_number                  INT     NOT NULL,
+Bedroom_number              INT,
+Max_people                  INT,
+Married_couples_allowed     BOOLEAN,
+Bed_number                  INT,
+Housing_type                VARCHAR(50),
+Occupation_status           BOOLEAN,
+Price_quarter		            INT,
 
 PRIMARY KEY (Building_number,Apt_number)
 );
@@ -70,7 +70,7 @@ FOREIGN KEY (Building_number,Apt_number) REFERENCES HOUSING_UNIT(Building_number
 CREATE TABLE APPLICANT (
 
 ID_number		CHAR(9)         NOT NULL,
-Acceptance_status      		    BOOLEAN,
+Staff_ID 		CHAR(9),
 
 PRIMARY KEY (ID_number),
 FOREIGN KEY (ID_number) REFERENCES USER (ID_number) on delete cascade,
@@ -78,30 +78,29 @@ FOREIGN KEY (Staff_ID) REFERENCES ADMINISTRATOR (Staff_ID) on delete cascade
 );
 
 CREATE TABLE HOUSING_PREFERENCE (
-  
-ID_number               CHAR(9)     NOT NULL,
-Building_preference     TINYINT,
-Housing_type_preference VARCHAR(50),
-Bedroom_preference	    TINYINT,
-Order_of_preference     TINYINT,
-Roommate_preference     VARCHAR(150),
+ID_number                   CHAR(9)       NOT NULL,
+Building_preference         TINYINT,
+Housing_type_preference     VARCHAR(50),
+Bedroom_preference	        TINYINT,
+Order_of_preference         TINYINT,
+Roommate_preference         VARCHAR(150),
 
 FOREIGN KEY (ID_number) REFERENCES APPLICANT(ID_number) on delete cascade
 );
 
 CREATE TABLE MAINTENANCE_REQUEST (
-  
-Request_number          INT          	NOT NULL,
-Resident_ID             CHAR(9)   		NOT NULL,
+Request_number          INT          	  NOT NULL,
+Resident_ID             CHAR(9)   		  NOT NULL,
 Admin_staff_ID          CHAR(9)   	    NOT NULL,
 Issue_desc              VARCHAR(150),
 Submission_date         VARCHAR(15),
 Date_completed          VARCHAR(15),
-Status_request          VARCHAR(10) 	NOT NULL CHECK(Status IN("pending", "Completed")),
+Status_request          VARCHAR(10) 	  NOT NULL CHECK(Status IN("pending", "Completed")),
 Comments                TEXT,
 
 PRIMARY KEY (Request_number),
 FOREIGN KEY (Resident_ID) REFERENCES RESIDENT(ID_number) on delete cascade,
 FOREIGN KEY (Admin_staff_ID) REFERENCES ADMINISTRATOR(Staff_ID) on delete cascade
+
 );
 
